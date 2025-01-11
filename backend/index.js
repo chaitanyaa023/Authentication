98a7136b73c82ce9fceb1170ffdf6eb959e813dc
chaitanyaa023 <chaitanya.egilam@gmail.com>
2025-01-11 13:29:53 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+
+dotenv.config();
 
 const app = express();
 
+app.get("/", (req,res)=> {
+    res.send("Hello World!");
+})
+
 app.listen(3000, () => {
+    connectDB();
     console.log("Server is running on port 3000");
 })
+// eW9Lin1fFUldNj6N
