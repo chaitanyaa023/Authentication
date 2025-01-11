3334ee6fd7e85e4c69a1fa257e4258b3b6a1f66a
chaitanyaa023 <chaitanya.egilam@gmail.com>
2025-01-11 14:48:03 +0530

    1st commit

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,21 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
 
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req,res)=> {
     res.send("Hello World!");
-})
+});
 
-app.listen(3000, () => {
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port:", PORT);
 })
 // eW9Lin1fFUldNj6N
