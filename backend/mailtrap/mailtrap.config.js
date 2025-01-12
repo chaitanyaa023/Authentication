8795da5f92b8ee868e6f55f0b4f2356fdb3b9217
chaitanyaa023 <chaitanya.egilam@gmail.com>
2025-01-12 13:06:29 +0530

    1st commit

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,30 @@
+import { MailtrapClient } from "mailtrap";
+import  dotenv  from "dotenv";
+
+dotenv.config();
+
+const TOKEN = process.env.MAILTRAP_ENDPOINT
+const ENDPOINT= process.env.MAILTRAP_ENDPOINT
+const client = new MailtrapClient({ endpoint: ENDPOINT,
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "chaitanya.egilam@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
